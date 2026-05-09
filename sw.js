4dcc84abf0a5a33c4479c53498e74748c156503d
sodztech <sodz5414@gmail.com>
2026-05-09 15:02:46 +0100

    update

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 //   - Google Fonts / CDN       → Cache First with long TTL
 //   - Everything else          → Network First
 
-var CACHE_NAME = 'qiblah-shell-v8';
+var CACHE_NAME = 'qiblah-shell-v24';
 
 var SHELL_ASSETS = [
   '/',
